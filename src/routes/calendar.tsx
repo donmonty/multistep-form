9639d7443abe673d646e8387798c45bddf9373cb
Montero <montero@Monteros-MacBook-Pro.local>
2022-06-23 11:36:44 -0500

    Added router

---
+++ src/routes/calendar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Calendar() {
+  return (
+    <main className="min-h-screen bg-gray-100 flex justify-center py-12 px-6 lg:px-8">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md min-h-full">
+        <h2>Calendar</h2>
+        
+      </div>
+    </main>
+  );
+}
